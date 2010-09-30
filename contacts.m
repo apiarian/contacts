1a8d54ea025e0a31639d19f446343aa0a129650b
Aleksandr Pasechnik <aleksandr.pasechnik@gmail.com>
2010-09-30 08:59:53 -0400

    Initial Commit

---
+++ contacts.m
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+#import <AddressBook/AddressBook.h>
+
+int main (int argc, const char * argv[]) {
+  NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+  
+  // insert code here...
+  NSLog(@"hello world!");
+  
+  [pool drain];
+  return 0;
+}
